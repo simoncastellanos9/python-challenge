3e7ecf8a36c6ef50a49b1c5cc929a141ec413c7c
Simon Castellanos <simoncastellanos9@gmail.com>
2021-05-03 12:42:18 -0500

    calculated all votes, assigned to each candidate. Created print for console correctly

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,9 +12,36 @@ with open(csvpath) as csvfile:
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
     #print(f"CSV Header: {csv_header}")
+    khan = 0
+    correy = 0
+    li = 0
+    otooley = 0
+    votes = 0
 
     for row in csvreader:
-        print(row)
+        votes = votes+1
+        if row[2] == 'Khan':
+            khan = khan + 1
+        elif row [2] == 'Correy':
+            correy = correy + 1
+        elif row[2] == 'Li':
+            li = li + 1
+        elif row[2 == "O'Tooley"]:
+            otooley = otooley + 1
+    
+    khanper = round((khan/votes)*100,3)
+    correyper = round((correy/votes)*100,3)
+    liper = round((li/votes)*100,3)
+    otooleyper = round((otooley/votes)*100,3)
+    
+
+    print("Election Results\n-------------------------")
+    print(f"Total Votes: {votes}")
+    print("-------------------------")
+    print(f"Khan: {khanper}% ({khan})")
+    print(f"Correy: {correyper}% ({correy})")
+    print(f"Li: {liper}% ({li})")
+    print(f"O'Tooley: {otooleyper}% ({otooley})")
 
 #output_path = os.path.join("analysis", "results.csv")
 
