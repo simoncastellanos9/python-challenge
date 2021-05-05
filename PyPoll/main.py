f92abf27ebfba33a3bd06505c0642974e04cf309
Simon Castellanos <simoncastellanos9@gmail.com>
2021-05-05 17:50:22 -0500

    modified csv path

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,8 @@
 import os
 import csv
 
-#csvpath = os.path.join('Resources','election_data.csv')
-csvpath = os.path.join('..','..','Downloads','election_data.csv')
+csvpath = os.path.join('Resources','election_data.csv')
+#csvpath = os.path.join('..','..','Downloads','election_data.csv')
 
 with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
