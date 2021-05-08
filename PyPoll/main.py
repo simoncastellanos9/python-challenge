ce385bcfe599a933b1a20515bf674558b1e75042
Simon Castellanos <simoncastellanos9@gmail.com>
2021-05-08 14:17:11 -0500

    removed testing code and comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,7 +2,6 @@ import os
 import csv
 
 csvpath = os.path.join('Resources','election_data.csv')
-#csvpath = os.path.join('..','..','Downloads','election_data.csv')
 
 with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
